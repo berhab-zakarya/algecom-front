c5ebdcf821cc376f2bd4817a01d02390f1b885c3
berhab-zakarya <zakarya.berhab@univ-tlemcen.dz>
2025-04-24 17:10:19 +0100

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,53 +1,104 @@
-// tailwind.config.ts
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
-        
+        // Primary Neutral Palette
         primary: {
-          1: 'var(--primary-1)',
-          2: 'var(--primary-2)',
-          3: 'var(--primary-3)',
-          4: 'var(--primary-4)',
-          5: 'var(--primary-5)',
-          6: 'var(--primary-6)',
-          7: 'var(--primary-7)',
-          8: 'var(--primary-8)',
+          DEFAULT: '#7C92FC',
+          900: '#1D317B',
+          800: '#2D3D92',
+          700: '#4226B5',
+          600: '#5E43DB',
+          500: '#7C92FC',
+          400: '#FFFFFF',
+          300: '#E5E5FF',
+          200: '#E7D9FE',
+          100: '#F7F4FF',
         },
-        
+        neutral: {
+          DEFAULT: '#FFFFFF',
+          900: '#171717',
+          800: '#262626',
+          700: '#404040',
+          600: '#525252',
+          500: '#737373',
+          400: '#A3A3A3',
+          300: '#D4D4D4',
+          200: '#E5E5E5',
+          100: '#F5F5F5',
+        },
+        // Success Palette
         success: {
-          DEFAULT: 'var(--success-default)',
-          dark: 'var(--success-dark)',
-          light: 'var(--success-light)',
+          DEFAULT: '#9CD323',
+          900: '#3B6506',
+          800: '#467A0B',
+          700: '#559711',
+          600: '#77B519',
+          500: '#9CD323',
+          400: '#BCE455',
+          300: '#C9F170',
+          200: '#DEFA95',
+          100: '#F2FED7',
         },
-        
+        // Error Palette
         error: {
-          DEFAULT: 'var(--error-default)',
-          dark: 'var(--error-dark)',
-          light: 'var(--error-light)',
+          DEFAULT: '#FF4423',
+          900: '#7A0919',
+          800: '#93081B',
+          700: '#B71112',
+          600: '#DB2719',
+          500: '#FF4423',
+          400: '#FF7759',
+          300: '#FFA37A',
+          200: '#FFCBA6',
+          100: '#FFE7D3',
         },
-        
+        // Warning Palette
         warning: {
-          DEFAULT: 'var(--warning-default)',
-          dark: 'var(--warning-dark)',
-          light: 'var(--warning-light)',
+          DEFAULT: '#FFC73A',
+          900: '#7A4005',
+          800: '#936312',
+          700: '#B7821D',
+          600: '#DBA32A',
+          500: '#FFC73A',
+          400: '#FFD98B',
+          300: '#FFE488',
+          200: '#FFEFBD',
+          100: '#FFEBD7',
         },
-        
+        // Information Palette
         info: {
-          DEFAULT: 'var(--info-default)',
-          dark: 'var(--info-dark)',
-          light: 'var(--info-light)',
+          DEFAULT: '#5AA6FF',
+          900: '#1D327A',
+          800: '#1A4395',
+          700: '#3A65B7',
+          600: '#5D81DB',
+          500: '#5AA6FF',
+          400: '#7CC2FF',
+          300: '#8BD3FF',
+          200: '#94E5FF',
+          100: '#DCF2FF',
         },
-        
+        // Secondary Palette
         secondary: {
-          DEFAULT: 'var(--secondary-default)',
-          dark: 'var(--secondary-dark)',
-          light: 'var(--secondary-light)',
-        }
+          DEFAULT: '#1A3D2C',
+          900: '#040815',
+          800: '#050C19',
+          700: '#0D121F',
+          600: '#131825',
+          500: '#1A3D2C',
+          400: '#5B6760',
+          300: '#A0A38F',
+          200: '#C5C6B3',
+          100: '#E5E6F4',
+        },
       },
       fontFamily: {
         algecom: ['var(--font-algecom)'],
